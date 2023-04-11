4c556b6eafe5824a80aef06a71e5e8e5a9f1354a
alif dragon <alifjr763@gmail.com>
2023-04-11 15:42:09 +0800

    login reg

--- main.js
+++ main.js
@@ -15,12 +15,65 @@ app.post('/reg', (req, res) => {
 })
 
 app.use(express.json())
-app.post('/login', (req, res) => {
-    console.log (req.body)
+// app.post('/login', (req, res) => {
+//     console.log (req.body)
 
-    res.send('login')
-})
+//     res.send('login')
+// })
+
+app.post('/register',(req,res)=> {
+  let result = register(
+    req.body.username,
+    req.body.password,
+    req.body.name,
+    req.body.email
+  )
+  res.send(result)
+  })
+
+
+  app.post('/login',(req,res)=> {
+    let result = login(
+      req.body.username,
+      req.body.password,
+    )
+    res.send(result)
+    })
+
+    function login(requsername,reqpassword){
+      let matchUser= dbUsers.find(
+       x => x.username == requsername
+        )
+    
+    if(!matchUser)return"User not found!"
+    if(matchUser.password == reqpassword){
+      return matchUser
+    
+    }else{
+      return "Invalid Password"
+    }
+    
+    }
+    
+    function register(reqUsername, reqPassword, reqName, reqEmail) {
+      dbUsers.push({
+          username: reqUsername,
+          password: reqPassword,
+          name: reqName,
+          email: reqEmail
+      })
+  }
+    
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+let dbUsers =[
+  {
+      username:"John Wick",
+      password:"123456",
+      name:"Wickwick",
+      email:"a@gmail.com",
+  },
+]
